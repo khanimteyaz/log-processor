3635a90c5ba6fd92651837f3504a0a05d7f82636
Imteyaz Khan <Imteyaz.Khan@jda.com>
2018-11-25 00:05:50 +0530

    log4j modification for more property and few exception type added for test

--- src/main/java/org/my/infra/log/controller/LogGeneratorController.java
+++ src/main/java/org/my/infra/log/controller/LogGeneratorController.java
@@ -5,6 +5,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
@@ -14,16 +15,22 @@ import java.io.BufferedReader;
 @RequestMapping("/log")
 public class LogGeneratorController {
 
-    private Logger LOGGER=LoggerFactory.getLogger(this.getClass());
+    private Logger LOGGER = LoggerFactory.getLogger(this.getClass());
 
     @RequestMapping(value = "/error", method = RequestMethod.POST)
-    public ResponseEntity<String> generateError() {
-        try
-        {
-            throw new IllegalArgumentException("Invalid input");
-        } catch(Exception ex) {
-            LOGGER.error("Error",ex);
+    public ResponseEntity<String> generateError(@RequestParam("type") int type) {
+        if (type == 1) {
+            try {
+                throw new IllegalArgumentException("Invalid input");
+            } catch (Exception ex) {
+                LOGGER.error("Error", ex);
+            }
+        } else if (type == 2) {
+            throw new NullPointerException("Jff");
+        } else if (type == 3) {
+            throw new RuntimeException("Unknown exception");
         }
+
         return ResponseEntity.ok("ok");
     }
 
